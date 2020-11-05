559260a6f6c6f6b3fbc6ed0d71dcaf3a17f0d759
Karlos Kelvin <karloskelvinsantos@gmail.com>
2020-11-05 00:13:54 -0300

    Resolve challenge of node fundamentals.

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
+const createTransactionService = new CreateTransactionService(transactionsRepository);
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    return response.json({
+      transactions: transactionsRepository.all(),
+      balance: transactionsRepository.getBalance()
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +21,23 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    if (type === 'outcome') {
+      const balance = transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw Error('Your balance is not sufficient for this transation!');
+      }
+    }
+    
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
